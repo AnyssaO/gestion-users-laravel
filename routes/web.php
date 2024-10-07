c262ed57206527339ab971868b9541e851ca7f84
AnyssaO <anyssa.ouahib@outlook.com>
2024-10-07 17:11:15 +0200

    added route to usercontroller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('users', UserController::class)->middleware('auth');
 
 Route::get('/', function () {
     return view('welcome');
