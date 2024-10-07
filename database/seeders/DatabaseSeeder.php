54542dd5bd3842a17f9e6dd437b2d00adb74dc1a
AnyssaO <anyssa.ouahib@outlook.com>
2024-10-07 16:32:57 +0200

    Populated DB with Demo Data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
+        \App\Models\User::factory(200)->create();
+    
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
