134575a2d18cceaca873047c83a46ab623a22b82
AnyssaO <anyssa.ouahib@outlook.com>
2024-10-07 16:56:38 +0200

    Added role column to user table in DB

---
+++ database/migrations/2024_10_07_144742_add_role_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //default = la value par défaut
+            $table->string('role')->default('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    // fonction de rollback pour annuler la migration
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+};
